c3935a377a5ba58d5d92e5183ec4222adf2f7cee
Lewis Dale <lewis.dale@madetech.com>
2019-11-29 10:30:02 +0000

    Added basic project structure, empty conifguration

--- src/main/java/com/learntech/horsefacts/application/HorsefactsApplication.java
+++ src/main/java/com/learntech/horsefacts/application/HorsefactsApplication.java
@@ -1,13 +1,11 @@
-package com.learntech.horsefacts;
+package com.learntech.horsefacts.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HorsefactsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HorsefactsApplication.class, args);
     }
-
 }
