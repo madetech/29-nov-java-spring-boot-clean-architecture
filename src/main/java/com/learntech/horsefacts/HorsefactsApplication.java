27fc2b2a9d15fa0a5fef7524967ca94fa158d5b4
Lewis Dale <lewis.dale@madetech.com>
2019-11-29 10:19:48 +0000

    Initial commit with Spring Initializr bootup

---
+++ src/main/java/com/learntech/horsefacts/HorsefactsApplication.java
@@ -0,0 +1,13 @@
+package com.learntech.horsefacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorsefactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorsefactsApplication.class, args);
+    }
+
+}
